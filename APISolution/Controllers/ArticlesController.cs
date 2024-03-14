734fb34216bc5737fd881ffdea0d1ee575fb3f88
Andra Arianti <ariantiandra@gmail.com>
2024-03-14 15:07:12 +0700

    initial commit

---
+++ APISolution/Controllers/ArticlesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL.DTOs;
+using MyWebFormApp.BLL.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APISolution.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ArticlesController : ControllerBase
+	{
+
+		private IArticleBLL _articleBLL;
+		public ArticlesController(IArticleBLL articleBLL)
+		{
+			_articleBLL = articleBLL;
+		}
+
+		// GET: api/<ArticlesController>
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var articles = _articleBLL.GetArticleWithCategory();
+			return Ok(articles);
+		}
+
+		// GET api/<ArticlesController>/5
+		[HttpGet("{id}")]
+		public ArticleDTO GetArticleById(int id)
+		{
+			return _articleBLL.GetArticleById(id);
+		}
+
+		// POST api/<ArticlesController>
+		[HttpPost]
+		public IActionResult Post(ArticleCreateDTO article)
+		{
+			_articleBLL.Insert(article);
+			return Ok();
+		}
+
+		// PUT api/<ArticlesController>/5
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] ArticleUpdateDTO article)
+		{
+			_articleBLL.Update(article);
+			return Ok();
+		}
+
+		// DELETE api/<ArticlesController>/5
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			_articleBLL.Delete(id);
+			return Ok($"Data Article {id} berhasil didelete");
+		}
+	}
+}
---
+++ APISolution/Controllers/CategoriesController.cs
@@ -0,0 +1,58 @@
+﻿using APISolution.Models;
+using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL.DTOs;
+using MyWebFormApp.BLL.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APISolution.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		private ICategoryBLL _categoryBLL;
+		public CategoriesController(ICategoryBLL categoryBLL)
+		{
+			_categoryBLL = categoryBLL;
+		}
+
+		// GET: api/Categories
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			return Ok(_categoryBLL.GetAll());
+		}
+
+		// GET api/Categories/5
+		[HttpGet("{id}")]
+		public CategoryDTO GetById(int id)
+		{
+			return _categoryBLL.GetById(id);
+		}
+
+		// POST api/<Categories_Controller>
+		[HttpPost]
+		public IActionResult Post(CategoryCreateDTO category)
+		{
+			_categoryBLL.Insert(category);
+			return Ok();
+		}
+
+		// PUT api/<Categories_Controller>/5
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] CategoryUpdateDTO category)
+		{
+			_categoryBLL.Update(category);
+			return Ok();
+		}
+
+		// DELETE api/<Categories_Controller>/5
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			_categoryBLL.Delete(id);
+			return Ok($"Data Category {id} berhasil didelete");
+		}
+	}
+}
